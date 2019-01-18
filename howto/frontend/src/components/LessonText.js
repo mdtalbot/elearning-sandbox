3adf85062e29b4a43ae23bd681eaa85156c85e4c
Matthew Talbot <mdtalbot@gmail.com>
2019-01-17 23:16:13 -0500

    separating components

---
+++ howto/frontend/src/components/LessonText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default function LessonText(props) {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+
