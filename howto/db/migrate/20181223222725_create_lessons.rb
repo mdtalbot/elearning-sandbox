fab7eb77c970b2b29d797617f234ec195cbb9b3d
Matthew Talbot <mdtalbot@gmail.com>
2019-02-04 16:03:54 -0500

    redefined schema

--- howto/db/migrate/20181223222725_create_lessons.rb
+++ howto/db/migrate/20181223222725_create_lessons.rb
@@ -8,13 +8,18 @@ class CreateLessons < ActiveRecord::Migration[5.2]
       t.string :subject
       t.string :decided_on
       t.string :decision
-      t.text :decided_by
-      t.text :argued_by
+      t.string :justices, array: true
+      t.string :plaintiff
+      t.string :defendant
+      t.string :plaintiff_att
+      t.string :defendant_att
       t.text :lesson_text_diff_adv
       t.text :lesson_text_diff_int
       t.text :lesson_text_diff_sim
-      t.string :oral_arg_one
-      t.string :oral_arg_two
+      t.string :oral_arg_link_one
+      t.string :oral_arg_link_two
+      t.string :oral_arg_text_one
+      t.string :oral_arg_text_two
       t.string :question_one
       t.string :question_two
       t.string :question_three
@@ -23,5 +28,6 @@ class CreateLessons < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :lessons, :justices
   end
 end
