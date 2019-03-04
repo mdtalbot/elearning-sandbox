a9fafe90ce188995c17662757be1e8cb5e34b02c
Matthew Talbot <mdtalbot@gmail.com>
2019-03-04 15:29:09 -0500

    adding exercises to lesson page

--- howto/frontend/src/components/LessonPage.js
+++ howto/frontend/src/components/LessonPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Segment, Image, Container, List, Grid } from 'semantic-ui-react'
+import { Header, Image, Grid, Form } from 'semantic-ui-react'
 import LessonSidebar from './LessonSidebar'
 import LessonText from './LessonText'
 
@@ -24,6 +24,49 @@ export default class LessonPage extends React.Component {
 
         <LessonText text={this.props.lesson.lesson_texts} />
 
+        <div className='answer-form'>
+          <Header textAlign='center'>Questions to Consider</Header>
+          <br />
+          <Grid centered container columns={2}>
+            <Grid.Column>
+              <Grid.Row>
+                <Form name="answer-input" onSubmit={this.handleSubmit}>
+                  <Form.TextArea className='answer-box' name='answerOne'
+                    value={this.state.answerOne} onChange={this.handleInputChange} style={{ minHeight: 150, maxHeight: 150 }} label="The Supreme Court of the United States has made decisions that give students fewer rights than adults would have in other settings. How do you think the Court justifies doing so?" >
+                  </Form.TextArea>
+                  <Form.Button>Submit</Form.Button>
+                </Form>
+              </Grid.Row>
+              <br />
+              <Grid.Row>
+                <Form name="answer-input" onSubmit={this.handleSubmit}>
+                  <Form.TextArea className='answer-box' name='answerThree'
+                    value={this.state.answerOne} onChange={this.handleInputChange} style={{ minHeight: 150, maxHeight: 150 }} label="While the Constitution doesn't mention a right to privacy, courts have repeatedly held that such a right exists. Do students and adults have the same right to privacy?" >
+                  </Form.TextArea>
+                  <Form.Button>Submit</Form.Button>
+                </Form>
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column>
+              <Grid.Row><Form name="answer-input" onSubmit={this.handleSubmit} >
+                <Form.TextArea className='answer-box' name='answerTwo'
+                  value={this.state.answerTwo} onChange={this.handleInputChange} style={{ minHeight: 150, maxHeight: 150 }} label="In a nonschool setting, are children's rights restricted in the same way that they are in a school setting? Why do you think this is the case?" >
+                </Form.TextArea>
+                <Form.Button>Submit</Form.Button>
+              </Form>
+              </Grid.Row>
+              <br />
+              <Grid.Row>
+                <Form name="answer-input" onSubmit={this.handleSubmit}>
+                  <Form.TextArea className='answer-box' name='answerFour'
+                    value={this.state.answerOne} onChange={this.handleInputChange} style={{ minHeight: 150, maxHeight: 150 }} label="The Supreme Court of the United States has made decisions that give students fewer rights than adults would have in other settings. How do you think the Court justifies doing so?" >
+                  </Form.TextArea>
+                  <Form.Button>Submit</Form.Button>
+                </Form>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
