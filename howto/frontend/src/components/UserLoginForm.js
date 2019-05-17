baabe7840908b78a587ee58965f1aa26e295089d
Matthew Talbot <mdtalbot@gmail.com>
2019-05-16 23:24:49 -0400

    login form updated for redux

--- howto/frontend/src/components/UserLoginForm.js
+++ howto/frontend/src/components/UserLoginForm.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { authenticate } from '../actions/authActions';
 import { Form } from 'semantic-ui-react'
 
-const propTypes = {};
-
-const defaultProps = {};
-
-export default class UserLoginForm extends React.Component {
+class UserLoginForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    if (this.props.authenticate(this.state)) {
+      this.props.history.push('/lessons')
+    } else {
+      window.alert("Sorry, something went wrong. Please try logging in again.")
+    }
   }
 
   render() {
     return (
-      <Form>
-        <Form.Input focus label='Username' />
-        <Form.Input focus label='Password' type='password' />
-        <Form.Button>Submit</Form.Button>
+      <Form onSubmit={this.handleLogin}>
+        <Form.Input focus label='E-mail' name="email" id='email' type="email"
+          onChange={this.handleChange} value={this.state.email}
+        />
+        <Form.Input focus label='Password' type='password' name='password' id='password'
+          onChange={this.handleChange} value={this.state.password}
+        />
+        <Form.Button type='submit'>Log In</Form.Button>
       </Form>
     );
   }
 }
 
- UserLoginForm.propTypes = propTypes;
- UserLoginForm.defaultProps = defaultProps;
+export default UserLoginForm = withRouter(connect(null, { authenticate })(UserLoginForm));
