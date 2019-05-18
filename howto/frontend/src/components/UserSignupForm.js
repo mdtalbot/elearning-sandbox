cadf496cc2ea486589c3f7371da8d543bf65a7cf
Matthew Talbot <mdtalbot@gmail.com>
2019-05-17 22:32:04 -0400

    signup form updated for redux

--- howto/frontend/src/components/UserSignupForm.js
+++ howto/frontend/src/components/UserSignupForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { signup } from '../actions/authActions';
 import { Button, Checkbox, Form, FormField, FormInput } from 'semantic-ui-react'
 
 
@@ -11,32 +14,49 @@ export default class UserSignupForm extends React.Component {
       last_name: '',
       email: '',
       password: '',
+      password_verify: '',
       is_teacher: false
     };
   }
 
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSignup = (event) => {
+    event.preventDefault();
+    if (this.props.signup(this.state)) {
+      this.props.history.push('/lessons')
+    } else {
+      window.alert("Sorry, something went wrong. Please try signing up again.")
+    }
+  }
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSignup}>
         <Form.Field>
           <label>Login</label>
-          <Form.Input placeholder='Enter the login name that you would like to use with this account.' />
+          <Form.Input name='username' id='username' type='text' value={this.state.login_name} onChange={this.handleChange} placeholder='Enter the login name that you would like to use with this account.' />
         </Form.Field>
         <Form.Field>
           <label>First name</label>
-          <Form.Input placeholder='Enter your first name.' />
+          <Form.Input name='first_name' id='first_name' type='text' value={this.state.first_name} onChange={this.handleChange} placeholder='Enter your first name.' />
         </Form.Field>
         <Form.Field>
           <label>Last name</label>
-          <Form.Input placeholder='Enter your last name.' />
+          <Form.Input name='last_name' id='last_name' type='text' value={this.state.last_name} onChange={this.handleChange} placeholder='Enter your last name.' />
         </Form.Field>
         <Form.Field>
           <label>Password</label>
-          <Form.Input type='password' placeholder='Enter a password.' />
+          <Form.Input type='password' name='password' id='password' value={this.state.password} onChange={this.handleChange} placeholder='Enter a password.' />
         </Form.Field>
         <Form.Field>
           <label>Re-enter password</label>
-          <Form.Input type='password' placeholder='Re-enter your password.' />
+          <Form.Input type='password' name='password_verify' id='password_verify' value={this.state.password_verify} onChange={this.handleChange} placeholder='Re-enter your password.' />
         </Form.Field>
         I am a:
         <Form.Field>
@@ -59,6 +79,7 @@ export default class UserSignupForm extends React.Component {
             onChange={this.handleChange}
           />
         </Form.Field>
+        <Button type='submit'>Create Account</Button>
       </Form>
     );
   }
